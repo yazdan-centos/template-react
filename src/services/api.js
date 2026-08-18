59d81ee5a32e57e395da91ad17ee7ddd77421dfe
yazdan-react <yazdanparast.react@gmail.com>
2026-08-18 03:57:02 -0800

    Update API base URL to production server

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE = process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080';
+const API_BASE = process.env.REACT_APP_API_BASE_URL || 'http://155.117.13.33:8080';
 const tokenKey = 'template_access_token';
 
 const api = axios.create({
